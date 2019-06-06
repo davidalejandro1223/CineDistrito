b060b60befb5331aeb140b7c7926fd4f0f20cae6
David Aparicio <davidalejandro1223@hotmail.com>
2019-06-05 21:21:15 -0500

    Corregidos serializers y view de funciones y peliculas

--- funciones/serializers.py
+++ funciones/serializers.py
@@ -1,12 +1,17 @@
 from .models import Pelicula, Funcion
 from rest_framework import serializers
 
-class PeliculaSerializer(serializers.HyperlinkedModelSerializer):
+class PeliculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pelicula
         fields = ('id', 'v_nombre', 'i_duracion', 'tx_sinapsis', 'v_foto')
 
-class FuncionSerializer(serializers.HyperlinkedModelSerializer):
+class FuncionPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Funcion
+        fields = ('id', 'v_estado', 'd_proyeccion', 'fk_pelicula', 't_inicioproyeccion', 't_finproyeccion')
+
+class FuncionGetSerializer(serializers.ModelSerializer):
     fk_pelicula = PeliculaSerializer()
     class Meta:
         model = Funcion
--- funciones/views.py
+++ funciones/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Pelicula, Funcion
 from rest_framework import viewsets
-from .serializers import PeliculaSerializer, FuncionSerializer
+from .serializers import PeliculaSerializer, FuncionGetSerializer, FuncionPostSerializer
 
 # Create your views here.
 
@@ -9,6 +9,12 @@ class PeliculaViewSet(viewsets.ModelViewSet):
     queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
 
+
 class FuncionViewSet(viewsets.ModelViewSet):
     queryset = Funcion.objects.all()
-    serializer_class = FuncionSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return FuncionPostSerializer
+        elif self.request.method == 'GET':
+            return FuncionGetSerializer
